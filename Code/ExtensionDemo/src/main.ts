0056264921e37e40699c3947468f4b6515a98db9
Lucas Pennati <pennal@usi.ch>
2017-04-02 16:52:01 +0200

    Added sample extension with Stack overflow parser

---
+++ Code/ExtensionDemo/src/main.ts
@@ -0,0 +1,251 @@
+/**
+ * Created by Lucas on 29.03.17.
+ */
+// MODELS
+abstract class LibraDocument {
+    abstract getParts(): Array<LibraPart>;
+}
+
+abstract class LibraPart {
+    private _units: Array<LibraInformationUnit>;
+    abstract getParts(): Array<LibraPart>;
+
+    constructor() {
+        this._units = new Array<LibraInformationUnit>();
+    }
+
+    addInformationUnit(unit: LibraInformationUnit): void {
+        this._units.push(unit);
+    }
+
+    getInformationUnits(): LibraInformationUnit[] {
+        return this._units;
+    }
+
+}
+
+class LibraInformationUnit {
+    tag: Array<string>;
+    parsedContent: string;
+}
+
+// Stack Overflow Models
+class SODocument extends LibraDocument {
+    question: SOQuestion;
+    answers: Array<SOAnswer>;
+
+    constructor() {
+        super();
+        this.answers = new Array<SOAnswer>();
+    }
+
+    getParts(): Array<LibraPart> {
+        return [this.question].concat(this.answers);
+    }
+
+    setQuestion(question: SOQuestion): void {
+        this.question = question;
+    }
+
+    setAnswers(answers: Array<SOAnswer>): void {
+        this.answers = answers;
+    }
+
+    addAnswer(answer: SOAnswer): void {
+        this.answers.push(answer);
+    }
+}
+
+class SOComment extends LibraPart  {
+
+    getParts(): LibraPart[] { return []; }
+
+}
+
+class SOPart extends LibraPart {
+    private _comments: Array<SOComment>;
+
+    constructor(comments: Array<SOComment>) {
+        super();
+        this._comments = [...comments];
+    }
+
+    getParts(): SOComment[] {
+        return this._comments;
+    }
+}
+
+class SOAnswer extends SOPart {
+
+}
+
+class SOQuestion extends SOPart  {
+
+}
+
+
+
+// DZONE
+// Stack Overflow Models
+// class DZoneDocument extends LibraDocument {
+//     units: SOQuestion;
+//     answers: Array<SOAnswer>;
+//
+//     constructor() {
+//         super();
+//         this.answers = new Array<SOAnswer>();
+//     }
+//
+//     getParts(): Array<LibraPart> {
+//         return [this.question].concat(this.answers);
+//     }
+//
+//     setQuestion(question: SOQuestion): void {
+//         this.question = question;
+//     }
+//
+//     setAnswers(answers: Array<SOAnswer>): void {
+//         this.answers = answers;
+//     }
+//
+//     addAnswer(answer: SOAnswer): void {
+//         this.answers.push(answer);
+//     }
+// }
+//
+// class SOComment extends LibraPart  {
+//
+//     getParts(): LibraPart[] { return []; }
+//
+// }
+//
+// class SOPart extends LibraPart {
+//     private _comments: Array<SOComment>;
+//
+//     constructor(comments: Array<SOComment>) {
+//         super();
+//         this._comments = [...comments];
+//     }
+//
+//     getParts(): SOComment[] {
+//         return this._comments;
+//     }
+// }
+//
+// class SOAnswer extends SOPart {
+//
+// }
+//
+// class SOQuestion extends SOPart  {
+//
+// }
+
+
+// BASE PARSER
+interface AbstractParser {
+    parse(): void;
+    getContent(): Array<LibraPart>;
+}
+
+// SO Parser
+class StackOverflowParser implements AbstractParser {
+    url: string;
+    rawContent: any;
+
+    document: SODocument;
+
+    constructor(url: string, rawContent: any) {
+        this.url = url;
+        this.rawContent = rawContent;
+        this.document = new SODocument();
+    }
+
+    parse(): void {
+        // Start by parsing the question
+        var questionPartElements = $('.question .postcell .post-text')[0].children;
+
+
+        // Create a Question Object
+        let soQuestion = new SOQuestion([]);
+
+        // Iterate over every paragraph of the question
+        for (var i = 0; i < questionPartElements.length; i++) {
+            let infoUnit: LibraInformationUnit = new LibraInformationUnit();
+            let elem = questionPartElements[i];
+
+            infoUnit.parsedContent = elem.textContent;
+
+            let tags = elem.getAttribute("class");
+
+            if (tags !== null) {
+                infoUnit.tag = tags.split(" ");
+            } else {
+                infoUnit.tag = ["plaintext"];
+            }
+
+            soQuestion.addInformationUnit(infoUnit);
+        }
+
+        this.document.setQuestion(soQuestion);
+
+        // Answers
+        var answers = $('.answer');
+
+        // Loop over all answers found
+        for (var i = 0; i < answers.length; i++) {
+            var answerParts = $(answers[i]).find('.answercell .post-text')[0].children;
+            let answer: SOAnswer = new SOAnswer([]);
+
+            for (var j = 0; j < answerParts.length; j++) {
+                let infoUnit: LibraInformationUnit = new LibraInformationUnit();
+                let elem = answerParts[j];
+                // Raw content
+                // infoUnit.rawContent = elem.innerHTML;
+                // parsed
+                infoUnit.parsedContent = elem.textContent;
+
+                let tags = elem.getAttribute("class");
+
+                if (tags !== null) {
+                    infoUnit.tag = tags.split(" ");
+                } else {
+                    infoUnit.tag = ["plaintext"];
+                }
+
+                answer.addInformationUnit(infoUnit);
+            }
+            this.document.addAnswer(answer);
+        }
+    }
+
+    getContent(): Array<LibraPart> {
+        return this.document.getParts();
+    }
+
+}
+
+// class DZoneParser implements AbstractParser {
+//     url: string;
+//     rawContent: any;
+//
+//     // document: DZoneDocument;
+// }
+
+$(document).ready(function() {
+    const EXTENSION_PREFIX = "LIBRA";
+
+
+    var url = window.location.href;
+    console.log("Extension is ready; URL: " + url);
+
+    let parser: AbstractParser;
+    if (url.indexOf("stackoverflow.com")) {
+        console.log("Calling StackOverflow Parser");
+        parser = new StackOverflowParser(url, document.textContent);
+    } else if (url.indexOf("dzone.com/tutorials/java")) {
+        console.log("Calling DZone Parser")
+    }
+    parser.parse();
+    console.log(parser.getContent());
+});
+
