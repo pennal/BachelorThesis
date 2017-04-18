408f078edb17239c93b7d8b08b67cd62ddfd3504
Lucas Pennati <pennal@usi.ch>
2017-04-18 17:35:45 +0200

    Fix to the build script

--- pullStormed.sh
+++ pullStormed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Before running, pull the dependency
-cd $HOME/Code/play-rest-api/lib
+cd $TRAVIS_BUILD_DIR/Code/play-rest-api/lib
 curl "https://stormed.inf.usi.ch/releases/ch/usi/inf/reveal/parsing/stormed-devkit/1.9.6/stormed-devkit-1.9.6.jar" -o "stormed-devkit-1.9.6.jar"
-cd $HOME/Code/play-rest-api/
+cd $TRAVIS_BUILD_DIR/Code/play-rest-api/
